081f08e4059fb2824cefa5ef4b7af097620a1106
BHammock33 <BennettHammock@gmail.com>
2023-11-16 12:00:46 -0500

    Finished up Crud functionality

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Http\Controllers\Controller;
 use App\Models\Listing;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
@@ -12,7 +12,7 @@ class ListingController extends Controller
 {
     public function index(){
         return view('listings.index', [
-            'listings' => Listing::latest()->filter(request(['tag', 'search']))->paginate(2)
+            'listings' => Listing::latest()->filter(request(['tag', 'search']))->paginate(8)
         ]);
     }
     public function show(Listing $listing){
@@ -33,8 +33,33 @@ class ListingController extends Controller
             'tags' => 'required',
             'description' => 'required'
         ]);
-
+        if($request->hasFile('logo')){
+            $formFields['logo'] = $request->file('logo')->store('logos', 'public');
+        }
         Listing::create($formFields);
         return Redirect::route('index')->with(['message' => 'Listing created successfully!']);
+    }
+    public function edit(Listing $listing){
+        return view('listings.edit', ['listing' => $listing]);
+    }
+    public function update(Request $request, Listing $listing){
+        $formFields = $request->validate([
+            'title' => 'required',
+            'company' => 'required',
+            'location' => 'required',
+            'website' => 'required',
+            'email' => ['required', 'email'],
+            'tags' => 'required',
+            'description' => 'required'
+        ]);
+        if($request->hasFile('logo')){
+            $formFields['logo'] = $request->file('logo')->store('logos', 'public');
+        }
+        $listing->update($formFields);
+        return back()->with(['message' => 'Listing updated successfully!']);
+    }
+    public function destroy(Listing $listing){
+        $listing->delete();
+        return redirect('/')->with('message', 'Listing deleted successfully');
     }
 }
